eea6e4efefee7cb18ae9b81a349b59f3bb8dc911
Bodakros <90177876+Bodakros@users.noreply.github.com>
2025-10-11 17:41:01 +0300

    + Added oauth2 via google (#52)

--- DjangoPart/Auth/urls.py
+++ DjangoPart/Auth/urls.py
@@ -1,11 +1,14 @@
 from django.contrib.auth.views import LogoutView
 from django.urls import path
 
+from . import views_oauth2
 from .views import MyLoginView, RegisterView, ProfileView
 
 app_name = 'Auth'
 urlpatterns = [
     path('', MyLoginView.as_view(), name='login'),
+    path('sign-out', views_oauth2.sign_out, name='sign_out'),
+    path('auth-receiver', views_oauth2.auth_receiver, name='auth_receiver'),
     path('logout', LogoutView.as_view(), name='logout'),
     path('register', RegisterView.as_view(), name='register'),
     path('profile', ProfileView.as_view(), name='profile')
